7846324091522e760d91bc4f361d059c20fc28f2
Odai-SiliconValley <shimashimanomi0422@ezweb.ne.jp>
2020-06-29 22:59:04 +0900

    ユーザー登録機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
+  end
   
 end
