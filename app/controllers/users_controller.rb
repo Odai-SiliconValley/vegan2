c08a2724b27b5b0fd2498d4233db54259dc1380a
Odai-SiliconValley <shimashimanomi0422@ezweb.ne.jp>
2020-06-30 21:54:41 +0900

    ログイン機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,5 +48,22 @@ class UsersController < ApplicationController
       render("users/edit")
     end
   end
+
+  def login_form
+  end
+
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+
+    if @user
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
   
 end
